ad6fda16511a4e60f23c25e97ca56b4ac86449d5
A.I <ailis@paw.zone>
2016-09-21 13:26:08 -0700

    another update test

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-setInterval(function() {
-  console.log("Hello World! 4");
-}, 5000);
+var http=require('http');
+var server=http.createServer();
+server.on('request', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World');
+  res.end();
+})
+server.listen(8080, '0.0.0.0');
