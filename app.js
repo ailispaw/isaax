f7355ad573268631b35efcbde44ac4089bf03f4d
A.I <ailis@paw.zone>
2016-09-21 15:16:32 -0700

    Blink ACT LED on board 10 times

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-var http=require('http');
-var server=http.createServer();
-server.on('request', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World');
-  res.end();
-})
-server.listen(8888, '0.0.0.0');
+var fs = require('fs');
+
+var isOn = false;
+var count = 0;
+var maxCount = 10;
+
+fs.writeFileSync('/sys/class/leds/led0/trigger', 'none');
+
+var blink = setInterval(function() {
+
+  if(count == maxCount) {
+    fs.writeFileSync('/sys/class/leds/led0/trigger', 'cpu0');
+    clearInterval(blink);
+    return;
+  }
+
+  if(isOn) {
+    fs.writeFileSync('/sys/class/leds/led0/brightness', '0');
+    isOn = false;
+  } else {
+    fs.writeFileSync('/sys/class/leds/led0/brightness', '1');
+    isOn = true;
+    count++;
+  }
+
+}, 1000);
